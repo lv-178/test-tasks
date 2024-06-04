a631c2b8e67fd584694af942f5890e8e175e0909
alisa <lv-178yandex.ru>
2024-06-04 14:28:47 +0300

    read json correctly

--- ReadJson/src/main/java/com/example/springexample/Flight.java
+++ ReadJson/src/main/java/com/example/springexample/Flight.java
@@ -2,8 +2,20 @@ package com.example.springexample;
 
 import lombok.Data;
 
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Data
 public class Flight {
-    private final String origin;
-
+    private String origin;
+    private String originName;
+    private String destination;
+    private String destinationName;
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+    private String carrier;
+    private long stops;
+    private long price;
 }
--- ReadJson/src/main/java/com/example/springexample/Main.java
+++ ReadJson/src/main/java/com/example/springexample/Main.java
@@ -8,13 +8,17 @@ import org.json.simple.parser.ParseException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws java.io.IOException, org.json.simple.parser.ParseException {
         ArrayList<Flight> flights = new ArrayList<>();
-        String directory = "data/test2.json";
-        String file = Files.readString(Path.of(directory));
+        String directory = "/home/administrator/Downloads/tickets.json";
+//        String file = Files.readString(Path.of(directory));
         JSONParser parser = new JSONParser();
 
         String content = new String(Files.readAllBytes(Paths.get(directory)));
@@ -30,7 +34,16 @@ public class Main {
             tickets.forEach(object1 -> {
                 try {
                     JSONObject jsonObjectFlight = (JSONObject) parser.parse(object1.toString());
-                    Flight flight = new Flight((String) jsonObjectFlight.get("origin"));
+                    Flight flight = new Flight(); //(String) jsonObjectFlight.get("origin")
+                    flight.setOrigin((String) jsonObjectFlight.get("origin"));
+                    flight.setOriginName((String) jsonObjectFlight.get("origin_name"));
+                    flight.setDestination((String) jsonObjectFlight.get("destination"));
+                    flight.setDestinationName((String) jsonObjectFlight.get("destination_name"));
+                    flight.setDepartureTime(LocalDateTime.of(LocalDate.parse(((String) jsonObjectFlight.get("departure_date")), DateTimeFormatter.ofPattern("dd.MM.yy")), LocalTime.parse(((String) jsonObjectFlight.get("departure_time")), DateTimeFormatter.ofPattern("[HH:mm][H:mm]"))));
+                    flight.setArrivalTime(LocalDateTime.of(LocalDate.parse(((String) jsonObjectFlight.get("arrival_date")), DateTimeFormatter.ofPattern("dd.MM.yy")), LocalTime.parse(((String) jsonObjectFlight.get("arrival_time")), DateTimeFormatter.ofPattern("[HH:mm][H:mm]"))));
+                    flight.setCarrier((String) jsonObjectFlight.get("carrier"));
+                    flight.setStops((long) jsonObjectFlight.get("stops"));
+                    flight.setPrice((long) jsonObjectFlight.get("price"));
                     flights.add(flight);
                 } catch (ParseException e) {
                     throw new RuntimeException(e);
